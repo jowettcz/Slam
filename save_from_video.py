19a236666b19a54dd7a09f7bde804a8f5ea58a60
jowett.zhang <jowett.zhang@gmail.com>
2020-05-05 13:43:53 +0800

    set input parameters

--- save_from_video.py
+++ save_from_video.py
@@ -1,19 +1,26 @@
 import cv2
+import argparse
 
-video_cap = cv2.VideoCapture('/Users/zhangzhaowei/Desktop/Drone/unvehicle/DJI_0004.MP4')
+if __name__ == '__main__':
 
-count = 0
-while True:
-	count += 1
-	success, image = video_cap.read()
-	print(success)
-	image.copyto(im)
-	im = cv2.resize(im, (0,0), 0.5, 0.5)
-	cv2.imshow("video",im)
-	while(cv2.waitKey(300) == 's'): # wait
-		image_name = ''+count+'.jpg';
-		cv2.imwrite(image_name, image)
+	parser = argparse.ArgumentParser() 
+	parser.add_argument("-i", "--file", help="input file", dest="input_file", required=True) 
+#	parser.add_argument("-o", "--output",help="output file location",dest="output_location",required=True)  
 
-video_cap.release() 
-cv2.destroyAllWindows() 
+	video_cap = cv2.VideoCapture(parser.input_file)
+
+	count = 0
+	while True:
+		count += 1
+		success, image = video_cap.read()
+		print(success)
+		image.copyto(im)
+		im = cv2.resize(im, (0,0), 0.5, 0.5)
+		cv2.imshow("video",im)
+		while(cv2.waitKey(300) == 's'): # wait
+			image_name = ''+count+'.jpg';
+			cv2.imwrite(image_name, image)
+
+	video_cap.release()
+	cv2.destroyAllWindows()
 
